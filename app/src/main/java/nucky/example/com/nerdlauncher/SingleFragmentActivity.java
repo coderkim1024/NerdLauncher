29f3951ab10a0eb18bc0ac13dc4f4aeff2e00f6a
coderkim1024 <coderkim1024@gmail.com>
2019-09-17 23:00:04 +0800

    Show all apps on the phone

---
+++ app/src/main/java/nucky/example/com/nerdlauncher/SingleFragmentActivity.java
@@ -0,0 +1,33 @@
+package nucky.example.com.nerdlauncher;
+
+import android.os.Bundle;
+import android.support.annotation.LayoutRes;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v7.app.AppCompatActivity;
+
+public abstract class SingleFragmentActivity extends AppCompatActivity {
+    //抽象创建Fragment方法,具体实现在子类
+    protected abstract Fragment createFragment();
+
+    //子类可以覆盖此方法，从而实现加载不同的layout
+    @LayoutRes
+    protected int getLayoutResId(){
+        return R.layout.activity_fragment;
+    }
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        //setContentView(R.layout.activity_fragment);
+        setContentView(getLayoutResId());
+
+        FragmentManager fm = getSupportFragmentManager();
+        Fragment fragment = fm.findFragmentById(R.id.fragment_container);
+        if(fragment == null){
+            fragment = createFragment();
+            fm.beginTransaction().add(R.id.fragment_container,fragment).commit();
+        }
+    }
+}
